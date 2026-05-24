ae1965686ada76d75cb35faa8250160d4915e3fa
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 22:48:09 +0700

    Update Passenger.java

--- MetroSystem/src/Metro/Passenger.java
+++ MetroSystem/src/Metro/Passenger.java
@@ -11,12 +11,8 @@ public class Passenger {
     private String verificationID;
     private double balance;
     private List<Ticket> tickets;
-
     // Constructor
-    public Passenger(String passengerId, String name,
-                     PassengerType passengerType,
-                     String verificationID,
-                     double balance) {
+    public Passenger(String passengerId, String name,PassengerType passengerType,String verificationID,double balance) {
         this.passengerId = passengerId;
         this.name = name;
         this.passengerType = passengerType;
@@ -24,18 +20,14 @@ public class Passenger {
         this.balance = balance;
         this.tickets = new ArrayList<>();
     }
-
     // Buy ticket
     public Ticket buyTicket(int stop, MetroLine line) {
         Ticket ticket = TicketFactory.factoryMethod(this, null, stop);
-
         if (ticket != null) {
             tickets.add(ticket);
         }
-
         return ticket;
     }
-
     // Get active ticket
     public Ticket getActiveTicket() {
         for (Ticket ticket : tickets) {
@@ -45,7 +37,6 @@ public class Passenger {
         }
         return null;
     }
-
     // Refund ticket
     public boolean refund(Ticket ticket) {
         if (ticket != null && tickets.contains(ticket)) {
@@ -55,39 +46,31 @@ public class Passenger {
         }
         return false;
     }
-
     // Top up balance
     public void topUp(double amount) {
         if (amount > 0) {
             balance += amount;
         }
     }
-
     // Getters & Setters
     public String getPassengerId() {
         return passengerId;
     }
-
     public void setPassengerId(String passengerId) {
         this.passengerId = passengerId;
     }
-
     public String getName() {
         return name;
     }
-
     public PassengerType getPassengerType() {
         return passengerType;
     }
-
     public String getVerificationID() {
         return verificationID;
     }
-
     public double getBalance() {
         return balance;
     }
-
     public List<Ticket> getTickets() {
         return tickets;
     }
