f9ce5ed0df327cf50958ec0ef0e2683072d9ed4b
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 19:10:07 +0700

    Update TicketFactory.java

--- MetroSystem/src/Metro/TicketFactory.java
+++ MetroSystem/src/Metro/TicketFactory.java
@@ -1,26 +1,9 @@
 package Metro;
 
-public class TicketFactory {
+public abstract class TicketFactory {
 	  // Factory Method
-    public abstract Ticket createTicket(
-            String ticketId,
-            double price,
-            Passenger passenger
-    );
-	    public static Ticket createTicket(TicketType type, String ticketId, double price, Passenger passenger) {
-	        switch (type) {
-	        
-	            case SINGLE:
-	                return new SingleTrip(ticketId, price, passenger);
-
-	            case DAILY:
-	                return new DayPass(ticketId, price, passenger);
-
-	            case MONTHLY:
-	                return new MonthlyPass(ticketId, price, passenger);
-
-	            default:
-	                throw new IllegalArgumentException("Invalid ticket type");
-	        }
-}
+ public abstract Ticket factoryMethod(
+	            Passenger pass,
+	            int stops
+	    );
 }
