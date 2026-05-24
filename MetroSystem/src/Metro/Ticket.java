f78c2990960fb9f1362f2ff017bb02e9e7106e4e
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 22:44:47 +0700

    Update Ticket.java

--- MetroSystem/src/Metro/Ticket.java
+++ MetroSystem/src/Metro/Ticket.java
@@ -3,7 +3,6 @@ package Metro;
 import java.time.LocalDateTime;
 
 public abstract class Ticket {
-
     protected String ticketId;
     protected TicketType type;
     protected TicketStatus status;
@@ -11,39 +10,25 @@ public abstract class Ticket {
     protected Passenger passenger;
     protected LocalDateTime purchasedAt;
     protected TicketState state;
-
-    public Ticket(
-            String ticketId,
-            TicketType type,
-            double price,
-            Passenger passenger
-    ) {
-
+    public Ticket(String ticketId, TicketType type, double price, Passenger passenger) {
         this.ticketId = ticketId;
         this.type = type;
         this.price = price;
         this.passenger = passenger;
-
         this.status = TicketStatus.ACTIVE;
-
         this.purchasedAt = LocalDateTime.now();
-
         // State mặc định
         this.state = new ActiveState();
     }
-
     // =========================
     // Getter
     // =========================
-
     public String getTicketId() {
         return ticketId;
     }
-
     public TicketType getType() {
         return type;
     }
-
     public TicketStatus getStatus() {
         return status;
     }
@@ -51,108 +36,60 @@ public abstract class Ticket {
     public double getPrice() {
         return price;
     }
-
     public Passenger getPassenger() {
         return passenger;
     }
-
     public LocalDateTime getPurchasedAt() {
         return purchasedAt;
     }
-
     public TicketState getState() {
         return state;
     }
-
     // =========================
     // Setter
     // =========================
-
-    public void setState(
-            TicketState state
-    ) {
-
+    public void setState(TicketState state) {
         this.state = state;
     }
-
     // =========================
     // Methods
     // =========================
-
     public boolean isValid() {
-
         return state.isValid();
     }
-
-    public abstract double calcPrice(
-            TicketType type
-    );
-
+    public abstract double calcPrice(TicketType type);
     public String generateQR() {
-
         return "QR-" + ticketId;
     }
-
     public void read() {
-
-        System.out.println(
-                "Reading ticket: "
-                + ticketId
-        );
+        System.out.println("Reading ticket: " + ticketId);
     }
-
     public boolean canRefund() {
-
         return state.canRefund();
     }
     // check-in
     public void checkIn() {
-
         if(state.isValid()) {
-
             state.handle(this);
-
         } else {
-
-            System.out.println(
-                    "Ticket invalid for check-in!"
-            );
+            System.out.println("Ticket invalid for check-in!");
         }
     }
-
     // check-out
     public void checkOut() {
-
         if(state instanceof UsedState) {
-
             state.handle(this);
-
         } else {
-
-            System.out.println(
-                    "Ticket invalid for check-out!"
-            );
+            System.out.println("Ticket invalid for check-out!");
         }
     }
     // refund
     public void refund() {
-
         if(state.canRefund()) {
-
-            setState(
-                    new RefundedState()
-            );
-
-            System.out.println(
-                    "Refund successful!"
-            );
-
+            setState(new RefundedState());
+            System.out.println("Refund successful!");
         } else {
-
-            System.out.println(
-                    "Ticket cannot be refunded!"
-            );
-            
+            System.out.println("Ticket cannot be refunded!");
 	public boolean isActive() {
 		// TODO Auto-generated method stub
 		return false;
