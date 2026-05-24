0c06cb3bd713bcfc9297fc59c886ccc0664fbff7
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 21:59:48 +0700

    Update PassengerType.java

--- MetroSystem/src/Metro/PassengerType.java
+++ MetroSystem/src/Metro/PassengerType.java
@@ -1,8 +1,21 @@
 package Metro;
 
 public enum PassengerType {
-	  NORMAL,
-	    STUDENT,
-	    SENIOR,
-	    DISABLE
+
+    NORMAL(1.0),
+    STUDENT(0.7),
+    SENIOR(0.5),
+    DISABLE(0.0);
+
+    private double rate;
+
+    PassengerType(double rate) {
+
+        this.rate = rate;
+    }
+
+    public double getRate() {
+
+        return rate;
+    }
 }
