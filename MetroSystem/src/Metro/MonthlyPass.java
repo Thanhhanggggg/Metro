955a885e5dc3e6dc154a1b63ca4a8924ffd3962a
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 22:46:58 +0700

    Update MonthlyPass.java

--- MetroSystem/src/Metro/MonthlyPass.java
+++ MetroSystem/src/Metro/MonthlyPass.java
@@ -4,68 +4,32 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class MonthlyPass extends Ticket {
-
     private List<MetroLine> validLines;
     private LocalDate validFrom;
     private LocalDate validUntil;
     private int rideCount;
     private int maxRides;
-
-    public MonthlyPass(
-            String ticketId,
-            Passenger passenger,
-            List<MetroLine> validLines
-    ) {
-
-        super(
-                ticketId,
-                TicketType.MONTHLY,
-                0,
-                passenger
-        );
-
+    public MonthlyPass(String ticketId, Passenger passenger, List<MetroLine> validLines) {
+        super(ticketId, TicketType.MONTHLY, 0, passenger );
         this.validLines = validLines;
-
         this.validFrom = LocalDate.now();
-
-        this.validUntil =
-                validFrom.plusMonths(1);
-
+        this.validUntil = validFrom.plusMonths(1);
         this.rideCount = 0;
-
         this.maxRides = 60;
-
-        this.price =
-                calcPrice(TicketType.MONTHLY);
+        this.price = calcPrice(TicketType.MONTHLY);
     }
-
     @Override
     public boolean isValid() {
-
-        return LocalDate.now()
-                .isBefore(validUntil)
-                && rideCount < maxRides
-                && getState().isValid();
+        return LocalDate.now().isBefore(validUntil) && rideCount < maxRides && getState().isValid();
     }
-
     @Override
-    public double calcPrice(
-            TicketType type
-    ) {
-
-        return FareConfig.getInstance()
-                .getFixedPriceMonthly();
+    public double calcPrice(TicketType type) {
+        return FareConfig.getInstance().getFixedPriceMonthly();
     }
-
     public double getRemainingRides() {
-
         return maxRides - rideCount;
     }
-
-    public boolean coverLine(
-            MetroLine line
-    ) {
-
+    public boolean coverLine(MetroLine line) {
         return validLines.contains(line);
     }
 }
