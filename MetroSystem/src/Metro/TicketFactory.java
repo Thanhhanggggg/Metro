e0cd9603ecb0f64797bbe1d771533a5c423a312a
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 20:19:13 +0700

    Update TicketFactory.java

--- MetroSystem/src/Metro/TicketFactory.java
+++ MetroSystem/src/Metro/TicketFactory.java
@@ -1,6 +1,74 @@
 package Metro;
 
 public class TicketFactory {
-	  // Factory Method
+	  package Metro;
+
+public class TicketFactory {
+    public Ticket factoryMethod(
+            Passenger passenger,
+            TicketType type,
+            int stops,
+            Station origin,
+            Station destination,
+            MetroLine line
+    ) 
+    {
+
+        double price =
+                FareConfig.getInstance()
+                        .calculateFare(
+                                stops,
+                                passenger.getPassengerType()
+                        );
+
+        switch (type) {
+
+            case SINGLE:
+
+                return new SingleTrip(
+                        generateTicketId(),
+                        price,
+                        passenger,
+                        origin,
+                        destination,
+                        line,
+                        stops
+                );
+
+            case DAILY:
+
+                return new DayPass(
+                        generateTicketId(),
+                        FareConfig.DAY_PASS_PRICE,
+                        passenger,
+                        origin,
+                        destination,
+                        line
+                );
+
+            case MONTHLY:
+
+                return new MonthlyPass(
+                        generateTicketId(),
+                        FareConfig.MONTHLY_PASS_PRICE,
+                        passenger,
+                        origin,
+                        destination,
+                        line
+                );
+
+            default:
+
+                throw new IllegalArgumentException(
+                        "Invalid Ticket Type"
+                );
+        }
+    }
+
+    private String generateTicketId() {
+
+        return "TK-" + System.currentTimeMillis();
+    }
+}
 	
 }
