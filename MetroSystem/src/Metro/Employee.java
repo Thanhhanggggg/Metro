0291c69091d28da1636d0e6dcf99fc2672212cfb
Thanhhanggggg <thanhhang29420066@gmail.com>
2026-05-24 22:13:45 +0700

    add Employee

--- MetroSystem/src/Metro/Employee.java
+++ MetroSystem/src/Metro/Employee.java
@@ -1,5 +1,36 @@
 package Metro;
 
 public abstract class Employee {
+	protected String employeeId;
+    protected String name;
+    protected String password;
+    public Employee(String employeeId, String name, String password) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.password = password;
+    }
+    public String getEmployeeId() {
+        return employeeId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    public boolean login(String inputPassword) {
+        if(password.equals(inputPassword)) {
+            System.out.println(name + " login successful!");
+            return true;
+        }
+        System.out.println("Login failed!");
+        return false;
+    }
+    
+    public void logout() {
+        System.out.println(name + " logged out!");
+    }
 
 }
