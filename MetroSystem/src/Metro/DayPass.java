262a1a49b7321ba852d4afd0220c42e2d8b5261d
TranNgan-07 <ngantran28072006@gmail.com>
2026-05-24 22:53:33 +0700

    Update DayPass.java

--- MetroSystem/src/Metro/DayPass.java
+++ MetroSystem/src/Metro/DayPass.java
@@ -3,52 +3,26 @@ package Metro;
 import java.time.LocalDate;
 
 public class DayPass extends Ticket {
-
     private LocalDate validDate;
     private int ridesUsed;
     private boolean unlimitedRides;
-
-    public DayPass(
-            String ticketId,
-            Passenger passenger
-    ) {
-
-        super(
-                ticketId,
-                TicketType.DAILY,
-                0,
-                passenger
-        );
-
+    public DayPass(String ticketId, Passenger passenger) {
+        super(ticketId, TicketType.DAILY, 0, passenger);
         this.validDate = LocalDate.now();
-
         this.ridesUsed = 0;
-
         this.unlimitedRides = true;
-
         this.price =
                 calcPrice(TicketType.DAILY);
     }
-
     @Override
     public boolean isValid() {
-
-        return LocalDate.now()
-                .equals(validDate)
-                && getState().isValid();
+        return LocalDate.now().equals(validDate) && getState().isValid();
     }
-
     @Override
-    public double calcPrice(
-            TicketType type
-    ) {
-
-        return FareConfig.getInstance()
-                .getFixedPriceDaily();
+    public double calcPrice(TicketType type) {
+        return FareConfig.getInstance().getFixedPriceDaily();
     }
-
     public void incrementRide() {
-
         ridesUsed++;
     }
 }
